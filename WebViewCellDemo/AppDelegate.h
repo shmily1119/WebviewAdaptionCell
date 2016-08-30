bde8f2fe03a852d65f21b7632c374547d5cabea5
夏勇 <yxia@bianguo.com.cn>
2016-08-30 23:40:10 +0800

    Initial Commit

---
+++ WebViewCellDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewCellDemo
+//
+//  Created by xiayong on 16/8/30.
+//  Copyright © 2016年 bianguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewCellDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebViewCellDemo
+//
+//  Created by xiayong on 16/8/30.
+//  Copyright © 2016年 bianguo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
